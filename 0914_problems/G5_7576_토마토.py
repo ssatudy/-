ca0683dab850b7e9d63bc221255beca1bd1845b5
ryuwc <wom05169@gmail.com>
2022-09-10 16:54:07 +0900

    Create G5_7576_토마토.py

---
+++ 0914_problems/G5_7576_토마토.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+def bfs(N, M):          # 여러개의 시작점을 갖는 bfs, 시작점에서의 거리를 탐색하며 알아냄
+    for i in range(N):
+        for j in range(M):
+            if tomato[i][j] == 1:
+                Q.append((i, j))
+
+    while Q:
+        i, j = Q.popleft()
+        for di, dj in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
+            nr, nc = i + di, j + dj
+            if 0 <= nr < N and 0 <= nc < M:
+                if tomato[nr][nc] == 0 and visited[nr][nc] == 0:
+                    Q.append((nr, nc))
+                    visited[nr][nc] = visited[i][j] + 1
+
+def result(N, M):
+    for i in range(N):
+        for j in range(M):
+            if tomato[i][j] == 0:
+                if visited[i][j] == 0:
+                    return print(-1)
+    ans = 0
+    for i in range(N):
+        for j in range(M):
+            if visited[i][j] > ans:
+                ans = visited[i][j]
+    return print(ans)
+
+M, N = map(int, sys.stdin.readline().split())
+
+tomato = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+Q = deque()
+visited = [[0]*M for _ in range(N)]
+bfs(N, M)
+
+result(N, M)
+
+# for line in visited:
+#     print(*line)
